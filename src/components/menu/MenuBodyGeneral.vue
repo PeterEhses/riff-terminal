78436fba9e73cfad9f7057501ca4bb0cce79a237
PeterEhses <ehsesp@hochschule-trier.de>
2021-07-06 10:36:09 +0200

    bugfixes, bump version

--- src/components/menu/MenuBodyGeneral.vue
+++ src/components/menu/MenuBodyGeneral.vue
@@ -17,42 +17,6 @@
         changes are saved!
       </p>
     </div>
-    <hr />
-    <div class="menu-general-section">
-      <p>Software, excluding media files, © 2021 Peter Ehses All Rights Reserved.</p>
-      <br />
-      <hr />
-      <br />
-      <p>
-        “Commons Clause” License Condition v1.0<br />
-        <br />
-        The Software, excluding media files, is provided to you by the Licensor under the License, as
-        defined below, subject to the following condition. <br />
-        <br />
-        Without limiting other conditions in the License, the grant of rights
-        under the License will not include, and the License does not grant to
-        you, the right to Sell the Software, excluding media files. <br />
-        <br />
-        For purposes of the foregoing, “Sell” means practicing any or all of the
-        rights granted to you under the License to provide to third parties, for
-        a fee or other consideration (including without limitation fees for
-        hosting or consulting/ support services related to the Software), a
-        product or service whose value derives, entirely or substantially, from
-        the functionality of the Software. Any license notice or attribution
-        required by the License must also include this Commons Clause License
-        Condition notice. <br />
-        <br />
-        Software: riff-terminal <br />
-        <br />
-        License, excluding media files: Apache-2.0
-        <br />
-        <br />
-        Licensor: Peter Ehses
-      </p>
-      <br />
-      <a href="https://commonsclause.com/">https://commonsclause.com/</a>
-      <a href="Apache 2.0 LICENSE-2.0.txt">https://www.apache.org/licenses/LICENSE-2.0.txt</a>
-    </div>
     <div class="EXIT-WARNING" v-if="exit"><h1>E X I T I N G</h1></div>
   </div>
 </template>
